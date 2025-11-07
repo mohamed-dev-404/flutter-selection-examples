9821c05d7b327176814c6fff6aebdc34dc0fd322
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-11-08 01:23:29 +0200

    init project

---
+++ android/app/src/main/kotlin/com/example/selections_examples/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.selections_examples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:selections_examples/root.dart';
+
+void main() {
+  runApp(const SelectionExamplesApp());
+}
+
+class SelectionExamplesApp extends StatelessWidget {
+  const SelectionExamplesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const RootView(),
+    );
+  }
+}
