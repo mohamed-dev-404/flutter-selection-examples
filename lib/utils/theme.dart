4a6b213f895a9abb82e55f2808fb189b98e228a3
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-11-08 17:58:03 +0200

    init app theme

---
+++ lib/utils/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme = ThemeData(
+  scaffoldBackgroundColor: Colors.white60,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.black87,
+    centerTitle: true,
+    titleTextStyle: TextStyle(
+      color: Colors.white70,
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+    ),
+    foregroundColor: Colors.blueAccent,
+  ),
+);
